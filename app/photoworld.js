77c3723874c9f2a0ae6788cddd1a58e50542ddb8
Rémi <remi.chaignon@gmail.com>
2012-07-08 18:26:32 -0600

    Update gl-matrix + Create shader script to compile all shader files in one json file

--- app/photoworld.js
+++ app/photoworld.js
@@ -7,6 +7,8 @@
 //
 // TOOLS
 // TEMPLATE
+// SHADER
+// WEBGL
 // PHOTO
 // AREA
 // WORLD
@@ -33,6 +35,12 @@
 
 	// Template classes holder
 	RC.template = {};
+
+	// Shaders classes holder
+	RC.shader = {};
+
+	// WebGL classes holder
+	RC.webgl = {};
 
 	// Photoworld classes holder
 	RC.photoworld = {};
@@ -237,6 +245,41 @@
 
 
 	///////////////////////////////////////////////////////////////////////////
+	// $SHADER
+
+	// Model
+	RC.shader.ShaderModel = Backbone.Model.extend({
+		defaults: {
+			_name: "error",
+			fragmentSource: "",
+			vertexSource: "",
+			programId: 0,
+			attribLocation: [],
+			uniformLocation: []
+		},
+		initialize: function() {}
+	});
+
+	// Collection
+	RC.shader.ShaderCollection = Backbone.Collection.extend({});
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// $WEBGL
+
+	// Model
+	RC.webgl.RendererModel = Backbone.Model.extend({});
+
+	// View
+	RC.webgl.RendererView = Backbone.View.extend({
+		el: "canvas",
+		events: {},
+		initialize: function() {},
+		render: function() {}
+	});
+
+
+	///////////////////////////////////////////////////////////////////////////
 	// $PHOTO
 
 	// Model
@@ -326,157 +369,157 @@
 			return;
 		}
 */
-	    var gl;
-	    function initGL(canvas) {
-	        try {
-	            gl = canvas.getContext("experimental-webgl");
-	            gl.viewportWidth = canvas.width;
-	            gl.viewportHeight = canvas.height;
-	        } catch (e) {
-	        }
-	        if (!gl) {
-	            alert("Could not initialise WebGL, sorry :-(");
-	        }
-	    }
+		var gl;
+		function initGL(canvas) {
+			try {
+				gl = canvas.getContext("experimental-webgl");
+				gl.viewportWidth = canvas.width;
+				gl.viewportHeight = canvas.height;
+			} catch (e) {
+			}
+			if (!gl) {
+				alert("Could not initialise WebGL, sorry :-(");
+			}
+		}
 
 
-	    function getShader(gl, id) {
-	        var shaderScript = document.getElementById(id);
-	        if (!shaderScript) {
-	            return null;
-	        }
+		function getShader(gl, id) {
+			var shaderScript = document.getElementById(id);
+			if (!shaderScript) {
+				return null;
+			}
 
-	        var str = "";
-	        var k = shaderScript.firstChild;
-	        while (k) {
-	            if (k.nodeType == 3) {
-	                str += k.textContent;
-	            }
-	            k = k.nextSibling;
-	        }
+			var str = "";
+			var k = shaderScript.firstChild;
+			while (k) {
+				if (k.nodeType == 3) {
+					str += k.textContent;
+				}
+				k = k.nextSibling;
+			}
 
-	        var shader;
-	        if (shaderScript.type == "x-shader/x-fragment") {
-	            shader = gl.createShader(gl.FRAGMENT_SHADER);
-	        } else if (shaderScript.type == "x-shader/x-vertex") {
-	            shader = gl.createShader(gl.VERTEX_SHADER);
-	        } else {
-	            return null;
-	        }
+			var shader;
+			if (shaderScript.type == "x-shader/x-fragment") {
+				shader = gl.createShader(gl.FRAGMENT_SHADER);
+			} else if (shaderScript.type == "x-shader/x-vertex") {
+				shader = gl.createShader(gl.VERTEX_SHADER);
+			} else {
+				return null;
+			}
 
-	        gl.shaderSource(shader, str);
-	        gl.compileShader(shader);
+			gl.shaderSource(shader, str);
+			gl.compileShader(shader);
 
-	        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-	            alert(gl.getShaderInfoLog(shader));
-	            return null;
-	        }
+			if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+				alert(gl.getShaderInfoLog(shader));
+				return null;
+			}
 
-	        return shader;
-	    }
-
-
-	    var shaderProgram;
-
-	    function initShaders() {
-	        var fragmentShader = getShader(gl, "shader-fs");
-	        var vertexShader = getShader(gl, "shader-vs");
-
-	        shaderProgram = gl.createProgram();
-	        gl.attachShader(shaderProgram, vertexShader);
-	        gl.attachShader(shaderProgram, fragmentShader);
-	        gl.linkProgram(shaderProgram);
-
-	        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-	            alert("Could not initialise shaders");
-	        }
-
-	        gl.useProgram(shaderProgram);
-
-	        shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-	        gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-	        shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-	        shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
-	    }
+			return shader;
+		}
 
 
-	    var mvMatrix = mat4.create();
-	    var pMatrix = mat4.create();
+		var shaderProgram;
 
-	    function setMatrixUniforms() {
-	        gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-	        gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-	    }
+		function initShaders() {
+			var fragmentShader = getShader(gl, "shader-fs");
+			var vertexShader = getShader(gl, "shader-vs");
 
+			shaderProgram = gl.createProgram();
+			gl.attachShader(shaderProgram, vertexShader);
+			gl.attachShader(shaderProgram, fragmentShader);
+			gl.linkProgram(shaderProgram);
 
+			if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+				alert("Could not initialise shaders");
+			}
 
-	    var triangleVertexPositionBuffer;
-	    var squareVertexPositionBuffer;
+			gl.useProgram(shaderProgram);
 
-	    function initBuffers() {
-	        triangleVertexPositionBuffer = gl.createBuffer();
-	        gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-	        var vertices = [
-	             0.0,  1.0,  0.0,
-	            -1.0, -1.0,  0.0,
-	             1.0, -1.0,  0.0
-	        ];
-	        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	        triangleVertexPositionBuffer.itemSize = 3;
-	        triangleVertexPositionBuffer.numItems = 3;
+			shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+			gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 
-	        squareVertexPositionBuffer = gl.createBuffer();
-	        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	        vertices = [
-	             1.0,  1.0,  0.0,
-	            -1.0,  1.0,  0.0,
-	             1.0, -1.0,  0.0,
-	            -1.0, -1.0,  0.0
-	        ];
-	        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	        squareVertexPositionBuffer.itemSize = 3;
-	        squareVertexPositionBuffer.numItems = 4;
-	    }
+			shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+			shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+		}
 
 
-	    function drawScene() {
-	        gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-	        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		var mvMatrix = mat4.create();
+		var pMatrix = mat4.create();
 
-	        mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
-
-	        mat4.identity(mvMatrix);
-
-	        mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
-	        gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-	        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	        setMatrixUniforms();
-	        gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-
-
-	        mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
-	        gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	        setMatrixUniforms();
-	        gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
-	    }
+		function setMatrixUniforms() {
+			gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+			gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+		}
 
 
 
-	    function webGLStart() {
-	        var canvas = document.getElementById("js-canvas");
-	        initGL(canvas);
-	        initShaders();
-	        initBuffers();
+		var triangleVertexPositionBuffer;
+		var squareVertexPositionBuffer;
 
-	        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	        gl.enable(gl.DEPTH_TEST);
+		function initBuffers() {
+			triangleVertexPositionBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+			var vertices = [
+				0.0, 1.0, 0.0,
+				-1.0, -1.0, 0.0,
+				1.0, -1.0, 0.0
+			];
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+			triangleVertexPositionBuffer.itemSize = 3;
+			triangleVertexPositionBuffer.numItems = 3;
 
-	        drawScene();
-	    }
+			squareVertexPositionBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+			vertices = [
+				1.0, 1.0, 0.0,
+				-1.0, 1.0, 0.0,
+				1.0, -1.0, 0.0,
+				-1.0, -1.0, 0.0
+			];
+			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+			squareVertexPositionBuffer.itemSize = 3;
+			squareVertexPositionBuffer.numItems = 4;
+		}
 
-	    webGLStart();
+
+		function drawScene() {
+			gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+			mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+			mat4.identity(mvMatrix);
+
+			mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
+			gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+			setMatrixUniforms();
+			gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+
+			mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
+			gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+			setMatrixUniforms();
+			gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+		}
+
+
+
+		function webGLStart() {
+			var canvas = document.getElementById("js-canvas");
+			initGL(canvas);
+			initShaders();
+			initBuffers();
+
+			gl.clearColor(0.0, 0.0, 0.0, 1.0);
+			gl.enable(gl.DEPTH_TEST);
+
+			drawScene();
+		}
+
+		webGLStart();
 
 		// Start router
 		RC.photoworld.router = new RC.photoworld.Router();
---
+++ shaders.py
@@ -0,0 +1,62 @@
+import os
+import glob
+import json
+import sys
+
+path = "app/shaders/"
+print "Path is " + path
+
+print "Removing previous json files..."
+for infile in glob.glob(os.path.join(path, "*.json")):
+    print "> Current file is: " + infile
+    os.remove(infile)
+
+
+html_escape_table = {
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&apos;",
+    ">": "&gt;",
+    "<": "&lt;",
+    }
+
+
+def html_escape(text):
+    return "".join(html_escape_table.get(c, c) for c in text)
+
+
+print "Building shader file"
+fshList = glob.glob(os.path.join(path, "*.fsh"))
+vshList = glob.glob(os.path.join(path, "*.vsh"))
+locList = glob.glob(os.path.join(path, "*.loc"))
+
+if (len(fshList) != len(vshList)) or (len(fshList) != len(locList)):
+    print "! Shader file missing..."
+    sys.exit(0)
+
+shader_dict = []
+for fname in fshList:
+    current_dict = {}
+    fname = fname[fname.rfind("/") + 1:fname.rfind(".")]
+    print "> Current shader is: " + fname
+    current_dict["_name"] = fname
+
+    fsh = glob.glob(os.path.join(path, fname + ".fsh"))[0]
+    current = open(fsh, "r+")
+    current_dict["fsh"] = html_escape(current.read())
+    current.close()
+
+    vsh = glob.glob(os.path.join(path, fname + ".vsh"))[0]
+    current = open(vsh, "r+")
+    current_dict["vsh"] = html_escape(current.read())
+    current.close()
+
+    loc = glob.glob(os.path.join(path, fname + ".loc"))[0]
+    current = open(loc, "r+")
+    current_dict["loc"] = html_escape(current.read())
+    current.close()
+
+    shader_dict.append(current_dict)
+shader = open(os.path.join(path, "shaders.json"), "w+")
+shader.write(json.dumps(shader_dict, indent=4))
+shader.close()
